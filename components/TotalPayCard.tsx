7beba434979afa5f73ec9da9c209f634f01af0ec
Isaac Hussain <isaaclearns58@gmail.com>
2024-08-03 02:51:18 +0300

    refactor: make payoutCard clickable

--- components/TotalPayCard.tsx
+++ components/TotalPayCard.tsx
@@ -1,26 +1,31 @@
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import React from "react";
 import UIText from "./ui/UIText";
 import LinearGradView from "./LinearGradView";
 import SummaryComponent from "./SummaryComponent";
+import { Link } from "expo-router";
 
 const monthlyPayout = 4800;
 
 const TotalPayCard = () => {
   return (
     <LinearGradView>
-      <View>
-        <UIText textStyles="text-textDark text-base">Monthly Payout</UIText>
-      </View>
-      <View className="flex-row items-center mb-4">
-        <UIText textStyles="text-textDark text-4xl font-bold">
-          SAR {monthlyPayout.toFixed(2)}
-        </UIText>
-      </View>
-      <View className="flex-row w-full px-4 justify-between">
-        <SummaryComponent label="Income" amount={558.0} />
-        <SummaryComponent label="Expenses" amount={356.0} />
-      </View>
+      <Link href="/StatsTab" asChild>
+        <TouchableOpacity className="py-7 px-2 items-center gap-y-2">
+          <View>
+            <UIText textStyles="text-textDark text-base">Monthly Payout</UIText>
+          </View>
+          <View className="flex-row items-center mb-4">
+            <UIText textStyles="text-textDark text-4xl font-bold">
+              SAR {monthlyPayout.toFixed(2)}
+            </UIText>
+          </View>
+          <View className="flex-row w-full px-4 justify-between">
+            <SummaryComponent label="Income" amount={558.0} />
+            <SummaryComponent label="Expenses" amount={356.0} />
+          </View>
+        </TouchableOpacity>
+      </Link>
     </LinearGradView>
   );
 };
